d2593acdca013fa1db5bc5276d32d3aa96ccbcc0
Jurgen S <thor78@gmx.at>
2012-11-20 07:56:48 +0100

    Changed arguments to work with NZBGet
    Reduced number of arguments and removed description not used arguments,
    set status of NZB to success

--- sabToCouchPotato.py
+++ sabToCouchPotato.py
@@ -3,22 +3,18 @@
 import sys
 import autoProcessMovie 
 
-if len(sys.argv) < 8: 
-    print "Not enough arguments received from SABnzbd." 
+if len(sys.argv) < 2: 
+    print "Not enough arguments received from NZBGet." 
     print "Running autoProcessMovie as a manual run"
     autoProcessMovie.process('Manual Run', 'Manual Run', 0)
 else:
-    status = int(sys.argv[7])
+    status = 0
     autoProcessMovie.process(sys.argv[1], sys.argv[2], status)
 
 
-# SABnzbd argv: 
+# NZBGet argv: 
 # 1  The final directory of the job (full path) 
 # 2  The original name of the NZB file 
-# 3  Clean version of the job name (no path info and ".nzb" removed) 
-# 4  Indexer's report number (if supported) 
-# 5  User-defined category 
-# 6  Group that the NZB was posted in e.g. alt.binaries.x 
-# 7  Status of post processing. 0 = OK, 1=failed verification, 2=failed unpack, 3=1+2
+# 3  Status of post processing. 0 = OK
 
 
