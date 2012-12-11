5d42efb7474e6fcca283a9fe79e30fe96feb836e
clinton-hall <fock_wulf@hotmail.com>
2012-12-10 16:28:30 -0800

    set to only send 4 arguments to postprocess script

--- nzbget-postprocessing-files/9.0/bin/nzbget-postprocess.sh
+++ nzbget-postprocessing-files/9.0/bin/nzbget-postprocess.sh
@@ -169,12 +169,12 @@ if [ "$NZBPP_PARSTATUS" -eq 1 -o "$NZBPP_PARSTATUS" -eq 3 -o "$NZBPP_PARFAILED" 
 		#if [ "$SickBeard" = "yes" -a "$NZBPP_CATEGORY" = "$SickBeardCategory" -a -e "$SabToSickBeard" ]; then
 		#	# Call SickBeard's postprocessing script
 		#	echo "[INFO] Post-Process: Running SickBeard's postprocessing script to notify Par-check failed"
-		#$PythonCmd $SabToSickBeard "$NZBPP_DIRECTORY" "$NZBPP_NZBFILENAME" "" "" "" "" "1" >/dev/null 2>&1
+		#$PythonCmd $SabToSickBeard "$NZBPP_DIRECTORY" "$NZBPP_NZBFILENAME" "1" >/dev/null 2>&1
 		#fi
 		if [ "$CouchPotato" = "yes" -a "$NZBPP_CATEGORY" = "$CouchPotatoCategory" -a -e "$nzbToCouchPotato" ]; then
 			# Call CouchPotato's postprocessing script
 			echo "[INFO] Post-Process: Running CouchPotato's postprocessing script to notify Par-check failed"
-			$PythonCmd $nzbToCouchPotato "$NZBPP_DIRECTORY" "$NZBPP_NZBFILENAME" "" "" "" "" "1" >/dev/null 2>&1
+			$PythonCmd $nzbToCouchPotato "$NZBPP_DIRECTORY" "$NZBPP_NZBFILENAME" "1" >/dev/null 2>&1
 		fi
 	fi
 	exit $POSTPROCESS_ERROR
@@ -243,12 +243,12 @@ if (ls *.rar >/dev/null 2>&1); then
 			#if [ "$SickBeard" = "yes" -a "$NZBPP_CATEGORY" = "$SickBeardCategory" -a -e "$SabToSickBeard" ]; then
 			#	# Call SickBeard's postprocessing script
 			#	echo "[INFO] Post-Process: Running SickBeard's postprocessing script to notify unrar (second pass) failed"
-			#	$PythonCmd $SabToSickBeard "$NZBPP_DIRECTORY" "$NZBPP_NZBFILENAME" "" "" "" "" "1">/dev/null 2>&1
+			#	$PythonCmd $SabToSickBeard "$NZBPP_DIRECTORY" "$NZBPP_NZBFILENAME" "1">/dev/null 2>&1
 			#fi
 			if [ "$CouchPotato" = "yes" -a "$NZBPP_CATEGORY" = "$CouchPotatoCategory" -a -e "$nzbToCouchPotato" ]; then
 				# Call CouchPotato's postprocessing script
 				echo "[INFO] Post-Process: Running CouchPotato's postprocessing script to notify unrar (second pass) failed"
-				$PythonCmd $nzbToCouchPotato "$NZBPP_DIRECTORY" "$NZBPP_NZBFILENAME" "" "" "" "" "1">/dev/null 2>&1
+				$PythonCmd $nzbToCouchPotato "$NZBPP_DIRECTORY" "$NZBPP_NZBFILENAME" "1">/dev/null 2>&1
 			fi
 		fi
 		exit $POSTPROCESS_ERROR
@@ -380,7 +380,7 @@ if [ "$SickBeard" = "yes" -a "$NZBPP_CATEGORY" = "$SickBeardCategory" -a -e "$Nz
 	# Call SickBeard's postprocessing script
 	echo "[INFO] Post-Process: Running SickBeard's postprocessing script"
 	# Uncomment below if your using development branch from fork https://github.com/Tolstyak/Sick-Beard.git
-	#$PythonCmd $NzbToSickBeard "$NZBPP_DIRECTORY" "$NZBPP_NZBFILENAME" "" "" "" "" "0" >/dev/null 2>&1
+	#$PythonCmd $NzbToSickBeard "$NZBPP_DIRECTORY" "$NZBPP_NZBFILENAME" "0" >/dev/null 2>&1
 	# If your using development branch from fork https://github.com/Tolstyak/Sick-Beard.git delete line below
 	$PythonCmd $NzbToSickBeard "$NZBPP_DIRECTORY" "$NZBPP_NZBFILENAME" >/dev/null 2>&1
 fi
@@ -388,7 +388,7 @@ fi
 if [ "$CouchPotato" = "yes" -a "$NZBPP_CATEGORY" = "$CouchPotatoCategory" -a -e "$NzbToCouchPotato" ]; then
 	# Call CouchPotato's postprocessing script
 	echo "[INFO] Post-Process: Running CouchPotato's postprocessing script"
-	$PythonCmd $NzbToCouchPotato "$NZBPP_DIRECTORY" "$NZBPP_NZBFILENAME" "" "" "" "" "0" >/dev/null 2>&1
+	$PythonCmd $NzbToCouchPotato "$NZBPP_DIRECTORY" "$NZBPP_NZBFILENAME" "0" >/dev/null 2>&1
 fi
 
 # Check if destination directory was set in postprocessing parameters
