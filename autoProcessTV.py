a749a6531deab6d678b5354f086a436fbf8c678f
clinton-hall <fock_wulf@hotmail.com>
2013-01-20 22:13:10 -0800

    Added print out for manual run.

--- autoProcessTV.py
+++ autoProcessTV.py
@@ -85,6 +85,10 @@ def processEpisode(dirName, nzbName=None, failed=False):
     except (ConfigParser.NoOptionError, ValueError):
         failed_fork = 0
     
+    #allows manual call of postprocess script if we have specified a watch_dir. Check that here.
+    if nzbName = "Manual Run" and watch_dir == ""
+        print "ERROR: In order to run this script manually you must specify a watch_dir in autoProcessTV.cfg"
+        sys.exit(-1)
     #allows us to specify the default watch directory and call the postproecssing on another PC with different directory structure.
     if watch_dir != "":
         dirName = watch_dir
