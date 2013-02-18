2ba29b45415aa09b2cb07a5977a97c581adbdc72
Joel Kåberg <joel.kaberg@gmail.com>
2013-02-18 14:35:47 +0100

    fixed some variables, small refactor
    if you're using Transmission in Windows use the .bat file

---
+++ TorrentToMedia.bat
@@ -0,0 +1,4 @@
+@echo off
+echo "Executing nzbToMedia..."
+C:\Python27\python.exe C:\Downloaders\nzbToMedia\TorrentToMedia.py %TR_TORRENT_DIR% %TR_TORRENT_NAME%
+pause
--- TorrentToMedia.py
+++ TorrentToMedia.py
@@ -16,7 +16,7 @@ import autoProcessTV
 from nzbToMediaEnv import *
 
 Logger = logging.getLogger()
-logFile = os.path.join(os.path.dirname(sys.argv[0]), "postprocess.log")
+logFile = os.path.normpath(os.path.join(os.path.dirname(sys.argv[0]), "postprocess.log"))
 logging.config.fileConfig(os.path.join(os.path.dirname(sys.argv[0]), "logger.conf"))
 fileHandler = logging.FileHandler(logFile, encoding='utf-8', delay=True)
 fileHandler.formatter = logging.Formatter('%(asctime)s|%(levelname)-7.7s %(message)s', '%H:%M:%S')
@@ -33,44 +33,45 @@ Logger.info("Loading config from %s", configFilename)
 if not os.path.isfile(configFilename):
 	Logger.error("You need an autoProcessMedia.cfg file - did you rename and edit the .sample?")
 	sys.exit(-1)
+else:
+	config.read(configFilename)
+	# Sick-Beard
+	tvCategory = config.get("SickBeard", "category")
+	tvDestination = os.path.normpath(config.get("SickBeard", "outputDirectory"))
+	# CouchPotato
+	movieDestination = os.path.normpath(config.get("CouchPotato", "outputDirectory"))
+	movieCategory = config.get("CouchPotato", "category")
+	useLink = int(config.get("Torrent", "uselink"))
+	extractionTool = os.path.normpath(config.get("Torrent", "extractiontool"))
 
-config.read(configFilename)
-
-TV_Cat = config.get("SickBeard", "category")
-TV_dest = os.path.normpath(config.get("SickBeard", "destination"))
-Movie_dest = os.path.normpath(config.get("CouchPotato", "destination"))
-Movie_Cat = config.get("CouchPotato", "category")
-useLink = int(config.get("Torrent", "uselink"))
-extractionTool = config.get("Torrent", "extractiontool")
-
-def category_search(Directory, Category, root):
-	DirBase = os.path.split(os.path.normpath(Directory)) #Test for blackhole sub-directory.
-	if DirBase[1] == Name:
+def category_search(inputDirectory, inputCategory, root):
+	categorySearch = os.path.split(os.path.normpath(inputDirectory)) #Test for blackhole sub-directory.
+	if categorySearch[1] == inputName:
 		Logger.info("Files appear to be in their own directory")
-		DirBase2 = os.path.split(os.path.normpath(DirBase[0]))
-		if DirBase2[1] == Movie_Cat or DirBase2[1] == TV_Cat:
-			if not Category:
-				Logger.info("Determined Category to be: %s", DirBase2[1])
-				Category = DirBase2[1]
-		elif not Category:
+		categorySearch2 = os.path.split(os.path.normpath(categorySearch[0]))
+		if categorySearch2[1] == movieCategory or categorySearch2[1] == tvCategory:
+			if not inputCategory:
+				Logger.info("Determined Category to be: %s", categorySearch2[1])
+				inputCategory = categorySearch2[1]
+		elif not inputCategory:
 			Logger.error("Could not identify category from the directory structure. please check downlaoder settings.")
 			sys.exit(-1)
 		else:
 			pass
 		
-	elif DirBase[1] == Movie_Cat or DirBase[1] == TV_Cat:
-		if os.path.isdir(os.path.join(Directory, Name)):
-			Logger.info("Found torrent directory %s in category directory %s", os.path.join(Directory, Name), Directory)
-			Directory = os.path.join(Directory, Name)
+	elif categorySearch[1] == movieCategory or categorySearch[1] == tvCategory:
+		if os.path.isdir(os.path.join(inputDirectory, inputName)):
+			Logger.info("Found torrent directory %s in category directory %s", os.path.join(inputDirectory, inputName), inputDirectory)
+			inputDirectory = os.path.join(inputDirectory, inputName)
 		else:
-			Logger.info("The directory passed is the root directory for category %s", DirBase[1])
+			Logger.info("The directory passed is the root directory for category %s", categorySearch[1])
 			Logger.warn("You should change settings to download torrents to their own directory if possible")
 			Logger.info("We will try and determine which files to process, individually")
 			root = 1
-		if not Category:
-			Logger.info("Determined Category to be: %s", DirBase[1])
-			Category = DirBase[1]
-	elif not Category:
+		if not inputCategory:
+			Logger.info("Determined Category to be: %s", categorySearch[1])
+			inputCategory = categorySearch[1]
+	elif not inputCategory:
 		Logger.error("Could not identify category from the directory structure. please check downlaoder settings.")
 		sys.exit(-1)
 	else:
@@ -78,35 +79,36 @@ def category_search(Directory, Category, root):
 		Logger.warn("You should change settings to download torrents to their own directory if possible and include Label/Category directories.")
 		Logger.info("We will try and determine which files to process, individually")
 		root = 1
-	return Directory, Category, root 
+	return inputDirectory, inputCategory, root 
 
-def is_sample(file_path, Name):
+def is_sample(file_path, inputName):
 	# 200 MB in bytes
 	SIZE_CUTOFF = 200 * 1024 * 1024
 	# ignore 'sample' in files unless 'sample' in Torrent Name
-	if ('sample' in file_path.lower()) and (not 'sample' in Name) and (os.path.getsize(file_path) < SIZE_CUTOFF):
+	if ('sample' in file_path.lower()) and (not 'sample' in inputName) and (os.path.getsize(file_path) < SIZE_CUTOFF):
 		return True
 	else:
 		return False
 
-def copy_link(source, target, useLink, destination):
+def copy_link(source, target, useLink, outputDestination):
 	## Create destination folder
-	if not os.path.exists(destination):
+	if not os.path.exists(outputDestination):
 		try:
-			Logger.debug("Creating destination folder: %s", destination)
-			os.makedirs(destination)
+			Logger.debug("Creating destination folder: %s", outputDestination)
+			os.makedirs(outputDestination)
 		except Exception, e:
 			Logger.error("Not possible to create destination folder: %s", e)
 			return False
+
 	if useLink:
 		try:
 			Logger.debug("Linking %s to %s", source, target)
 			linktastic.link(source, target)
 		except:
 			if os.path.isfile(target):
-				Logger.info("something went wrong in linktastic.link, but the destination file was created")
+				Logger.info("Something went wrong in linktastic.link, but the destination file was created")
 			else:
-				Logger.info("something went wrong in linktastic.link. trying to create a copy")
+				Logger.info("Something went wrong in linktastic.link. trying to create a copy")
 				Logger.debug("Copying %s to %s", source, target)
 				shutil.copy(source, target)
 	else:
@@ -114,7 +116,7 @@ def copy_link(source, target, useLink, destination):
 		shutil.copy(source, target)
 	return True
 
-def unpack(dirpath, file, destination):
+def unpack(dirpath, file, outputDestination):
 	## Using Windows?
 	if os.name == 'nt':
 		cmd_7zip = [extractionTool, 'x -y']
@@ -157,30 +159,30 @@ def unpack(dirpath, file, destination):
 			return False
 
 	## Create destination folder
-	if not os.path.exists(destination):
+	if not os.path.exists(outputDestination):
 		try:
-			Logger.debug("Creating destination folder: %s", destination)
-			os.makedirs(destination)
+			Logger.debug("Creating destination folder: %s", outputDestination)
+			os.makedirs(outputDestination)
 		except Exception, e:
 			Logger.error("Not possible to create destination folder: %s", e)
 			return False
 
-	Logger.info("Extracting %s to %s", fp, destination)
+	Logger.info("Extracting %s to %s", fp, outputDestination)
 
 	## Running..	
-	Logger.debug("Extracting %s %s %s %s", cmd[0], cmd[1], fp, destination)
+	Logger.debug("Extracting %s %s %s %s", cmd[0], cmd[1], fp, outputDestination)
 	pwd = os.getcwd() # Get our Present Working Directory
-	os.chdir(destination) #not all unpack commands accept full paths, so just extract into this directory.
+	os.chdir(outputDestination) #not all unpack commands accept full paths, so just extract into this directory.
 	if os.name == 'nt': #Windows needs quotes around directory structure
 		try:
 			run = "\"" + cmd[0] + "\" " + cmd[1] + " \"" + fp + "\"" #windows needs quotes around directories.
 			res = call(run)
 			if res == 0:
-				Logger.info("Extraction was successful for %s to %s", fp, destination)
+				Logger.info("Extraction was successful for %s to %s", fp, outputDestination)
 			else:
 				Logger.info("Extraction failed for %s. 7zip result was %s", fp, res)
 		except:
-			Logger.error("Extraction failed for %s. Could not call command %s %s", fp, run)
+			Logger.error("Extraction failed for %s. Could not call command %s", fp, run)
 	else:
 		try:
 			if cmd[1] == "": #if calling unzip, we dont want to pass the ""
@@ -188,7 +190,7 @@ def unpack(dirpath, file, destination):
 			else:
 				res = call([cmd[0], cmd[1], fp])
 			if res == 0:
-				Logger.info("Extraction was successful for %s to %s", fp, destination)
+				Logger.info("Extraction was successful for %s to %s", fp, outputDestination)
 			else:
 				Logger.error("Extraction failed for %s. 7zip result was %s", fp, res)
 		except:
@@ -196,17 +198,17 @@ def unpack(dirpath, file, destination):
 	os.chdir(pwd) # Go back to our Original Working Directory
 	return True
 
-def flatten(destination):
-	Logger.info("Flattening directory: %s", destination)
-	for dirpath, dirnames, filenames in os.walk(destination): #flatten out the directory to make postprocessing easier.
-		if dirpath == destination:
+def flatten(outputDestination):
+	Logger.info("Flattening directory: %s", outputDestination)
+	for dirpath, dirnames, filenames in os.walk(outputDestination): #flatten out the directory to make postprocessing easier.
+		if dirpath == outputDestination:
 			continue #no need to try and move files in the root destination directory.
 		for filename in filenames:
 			try:
-				shutil.move(os.path.join(dirpath, filename), destination)
+				shutil.move(os.path.join(dirpath, filename), outputDestination)
 			except OSError:
 				Logger.info("Could not flatten %s", os.path.join(dirpath, filename))
-	removeEmptyFolders(destination) #cleanup empty directories.
+	removeEmptyFolders(outputDestination) #cleanup empty directories.
 
 def removeEmptyFolders(path):
 	Logger.info("Removing empty folders in: %s", path)
@@ -227,135 +229,86 @@ def removeEmptyFolders(path):
 		Logger.debug("Removing empty folder: %s", path)
 		os.rmdir(path)
 
-if len(sys.argv) == 4:
-	##You can use the following parameters (UTORRENT):
-	##
-	##%F - Name of downloaded file (for single file torrents)
-	##%D - Directory where files are saved
-	##%N - Title of torrent
-	##%P - Previous state of torrent
-	##%L - Label
-	##%T - Tracker
-	##%M - Status message string (same as status column)
-	##%I - hex encoded info-hash
-	##%S - State of torrent
-	##%K - kind of torrent (single|multi)
-	##
-	##Where State is one of:
-	##
-	##Error - 1
-	##Checked - 2
-	##Paused - 3
-	##Super seeding - 4
-	##Seeding - 5
-	##Downloading - 6
-	##Super seed [F] - 7
-	##Seeding [F] - 8
-	##Downloading [F] - 9
-	##Queued seed - 10
-	##Finished - 11
-	##Queued - 12
-	##Stopped - 13
-	
-	## We will pass in %D, %N, %L from uTorrent
-	Logger.info("Script called from utorrent")
-	Directory = os.path.normpath(sys.argv[1])	## %D -- Example output: F:\path\to\dir\My.Series.S01E01.720p.HDTV.x264-2HD
-	Name = sys.argv[2]		## %N -- Example output: My.Series.S01E01.720p.HDTV.x264-2HD
-	Category = sys.argv[3]	## %L -- Example output: tvseries ## This is the label in uTorrent
-
-elif len(sys.argv) > 1: #Doesn't match Transmission (1) or uTorrent (4).
-	Logger.error("The number of arguments passed is %s. Unable to determin the arguments to use; Exiting", len(sys.argv))
+if len(sys.argv) == 3:
+	## We will pass in %D, %N from uTorrent, or %TR_TORRENT_DIR% %TR_TORRENT_NAME% from Transmission
+	inputDirectory = os.path.normpath(sys.argv[1])
+	inputName = sys.argv[2]
+	inputCategory = ''	# We dont have a category, so assume the last directory is the category for now.
+else:
+	Logger.error("There was a problem loading variables: Exiting")
 	sys.exit(-1)
 
-else:
-	##test for Transmission here.
-	#TR_APP_VERSION
-	#TR_TIME_LOCALTIME
-	#TR_TORRENT_DIR
-	#TR_TORRENT_HASH
-	#TR_TORRENT_ID
-	#TR_TORRENT_NAME
-	try:
-		Directory = os.path.normpath(os.getenv('TR_TORRENT_DIR'))
-		Name = os.getenv('TR_TORRENT_NAME')
-		Logger.info("Script called from Transmission")
-	except:
-		Logger.error("There was a problem loading variables from Transmission: Exiting")
-		sys.exit(-1)
-	Category = '' #We dont have a category, so assume the last directory is the category for now.
-	
-Logger.debug("Received Directory: %s", Directory)
-Logger.debug("Received Torrent Name: %s", Name)
-Logger.debug("Received Category: %s", Category)
+Logger.debug("Received Directory: %s | Name: %s", inputDirectory, inputName)
 
 status = 1 # we start as "failed" until we verify movie file in destination
 root = 0
 video = 0
 video2 = 0
 
-Directory, Category, root = category_search(Directory, Category, root) # confirm the catgeogy by parsing directory structure.
-		
-if Category == Movie_Cat:
-	destination = os.path.join(Movie_dest, Name)
-elif Category == TV_Cat:
-	destination = os.path.join(TV_dest, Name)
+inputDirectory, inputCategory, root = category_search(inputDirectory, inputCategory, root) # confirm the catgeogy by parsing directory structure.
+
+if inputCategory == movieCategory:
+	outputDestination = os.path.normpath(os.path.join(movieDestination, inputName))
+elif inputCategory == tvCategory:
+	outputDestination = os.path.normpath(os.path.join(tvDestination, inputName))
 else:
-	Logger.info("Category of %s does not match either %s or %s: Exiting", Category, Movie_Cat, TV_Cat)
+	Logger.info("Category of %s does not match either %s or %s: Exiting", inputCategory, movieCategory, tvCategory)
 	sys.exit(-1)
 
 packed_files = ['.zip', '.rar', '.7z', '.gz', '.bz', '.tar', '.arj']
 video_files = ['.mkv', '.avi', '.divx', '.xvid', '.mov', '.wmv', '.mp4', '.mpg', '.mpeg', '.vob', '.iso']
 meta_files = ['.nfo', '.sub', '.srt', '.jpg', '.gif']
-Logger.debug("scanning files in directory: %s", Directory)
 
-for dirpath, dirnames, filenames in os.walk(Directory):
+Logger.debug("Scanning files in directory: %s", inputDirectory)
+
+for dirpath, dirnames, filenames in os.walk(inputDirectory):
 	for file in filenames:
 		if root == 1:
-			Logger.debug("Looking for %s in filename", Name)
-			if (Name in file) or (file in Name):
+			Logger.debug("Looking for %s in filename", inputName)
+			if (inputName in file) or (file in inputName):
 				pass #This file does match the Torrent name
 			else:
 				continue #This file does not match the Torrent name. Skip it
 		file_path = os.path.join(dirpath, file)
 		file_ext = os.path.splitext(file)[1]
 		if file_ext in video_files: #if the file is a video file.
-			if is_sample(file_path, Name):
+			if is_sample(file_path, inputName):
 				Logger.info("file %s is a sample file. Ignoring", file_path)
 				continue #ignore samples
 			video = video + 1
 			source = file_path
-			target = os.path.join(destination, file)
+			target = os.path.join(outputDestination, file)
 			Logger.info("Found video file %s.", file)
-			state = copy_link(source, target, useLink, destination)
+			state = copy_link(source, target, useLink, outputDestination)
 			if state == False:
 				Logger.info("Failed to link file %s.", file)
 		elif file_ext in meta_files:
 			source = file_path
-			target = os.path.join(destination, file)
+			target = os.path.join(outputDestination, file)
 			Logger.info("Found metadata file %s.", file)
-			state = copy_link(source, target, useLink, destination)
+			state = copy_link(source, target, useLink, outputDestination)
 			if state == False:
 				Logger.info("Failed to link file %s.", file)
 		elif file_ext in packed_files:
 			Logger.info("Found packed file %s.", file)
 			source = file_path
-			target = os.path.join(destination, file)
-			state = unpack(dirpath, file, destination)
+			target = os.path.join(outputDestination, file)
+			state = unpack(dirpath, file, outputDestination)
 			if state == False:
 				Logger.info("Failed to unpack file %s.", file)
 		else:
 			Logger.info("Unknown file type %s for file %s. Ignoring", file_ext, file_path)
 			continue
-flatten(destination)
+flatten(outputDestination)
 
 #now check if movie files exist in destination:
-for dirpath, dirnames, filenames in os.walk(destination):
+for dirpath, dirnames, filenames in os.walk(outputDestination):
 	for file in filenames:
 		file_path = os.path.join(dirpath, file)
 		file_ext = os.path.splitext(file)[1]
 		if file_ext in video_files: #if the file is a video file.
 			video2 = video2 + 1
-if video2 >= video and video2 > 0:	#check that all video files were moved.		
+if video2 >= video and video2 > 0:	#check that all video files were moved.
 	status = 0
 
 status = int(status)
@@ -365,7 +318,7 @@ else:
 	Logger.info("calling autoProcess script for failed download")
 ## Now we pass off to CouchPotato or SickBeard.
 # still need to figure out how to log this output.
-if Category == Movie_Cat:  
-	autoProcessMovie.process(destination, Name, status)
-elif Category == TV_Cat:
-	autoProcessTV.processEpisode(destination, Name, status)
+if inputCategory == movieCategory:  
+	autoProcessMovie.process(outputDestination, inputName, status)
+elif inputCategory == tvCategory:
+	autoProcessTV.processEpisode(outputDestination, inputName, status)
