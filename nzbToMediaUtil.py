8acc9e030b3a95ee2c2d9eaf47d7eb9b1d179562
Clinton Hall <fock_wulf@hotmail.com>
2013-02-19 21:24:08 -0800

    fixed indent

--- nzbToMediaUtil.py
+++ nzbToMediaUtil.py
@@ -1,5 +1,4 @@
 
-
 import logging
 import logging.config
 import os.path
@@ -7,8 +6,8 @@ import os.path
 
 def nzbtomedia_configure_logging(dirname):
   logFile = os.path.join(dirname, "postprocess.log")
-	logging.config.fileConfig(os.path.join(dirname, "logger.conf"))
-	fileHandler = logging.FileHandler(logFile, encoding='utf-8', delay=True)
-	fileHandler.formatter = logging.Formatter('%(asctime)s|%(levelname)-7.7s %(message)s', '%H:%M:%S')
-	fileHandler.level = logging.DEBUG
-	logging.getLogger().addHandler(fileHandler)
+  logging.config.fileConfig(os.path.join(dirname, "logger.conf"))
+  fileHandler = logging.FileHandler(logFile, encoding='utf-8', delay=True)
+  fileHandler.formatter = logging.Formatter('%(asctime)s|%(levelname)-7.7s %(message)s', '%H:%M:%S')
+  fileHandler.level = logging.DEBUG
+  logging.getLogger().addHandler(fileHandler)
