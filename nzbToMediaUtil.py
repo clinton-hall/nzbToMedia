2bbedfae6593fed9682c412c4f0ee2e823799413
Clinton Hall <fock_wulf@hotmail.com>
2013-02-19 21:15:30 -0800

    #65

---
+++ nzbToMediaUtil.py
@@ -0,0 +1,14 @@
+
+
+import logging
+import logging.config
+import os.path
+
+
+def nzbtomedia_configure_logging(dirname):
+  logFile = os.path.join(dirname, "postprocess.log")
+	logging.config.fileConfig(os.path.join(dirname, "logger.conf"))
+	fileHandler = logging.FileHandler(logFile, encoding='utf-8', delay=True)
+	fileHandler.formatter = logging.Formatter('%(asctime)s|%(levelname)-7.7s %(message)s', '%H:%M:%S')
+	fileHandler.level = logging.DEBUG
+	logging.getLogger().addHandler(fileHandler)
