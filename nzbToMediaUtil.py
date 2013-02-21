a11619b03c58b8b5386af24c6ff0da0aaf30c60b
Clinton Hall <fock_wulf@hotmail.com>
2013-02-21 13:20:05 -0800

    Fix log config reading.

--- nzbToMediaUtil.py
+++ nzbToMediaUtil.py
@@ -1,4 +1,3 @@
-
 import logging
 import logging.config
 import os.path
@@ -6,7 +5,7 @@ import os.path
 
 def nzbtomedia_configure_logging(dirname):
   logFile = os.path.join(dirname, "postprocess.log")
-  logging.config.fileConfig(os.path.join(dirname, "logger.conf"))
+  logging.config.fileConfig(os.path.join(dirname, "autoProcessMedia.cfg"))
   fileHandler = logging.FileHandler(logFile, encoding='utf-8', delay=True)
   fileHandler.formatter = logging.Formatter('%(asctime)s|%(levelname)-7.7s %(message)s', '%H:%M:%S')
   fileHandler.level = logging.DEBUG
