f532091de592c39cfd863e799d0f699430d8433a
Clinton Hall <fock_wulf@hotmail.com>
2013-03-03 17:54:29 -0800

    Get result back from autoProcessMovie
    We don't do anything with it... yet ;)

--- nzbToCouchPotato.py
+++ nzbToCouchPotato.py
@@ -24,7 +24,7 @@ if len(sys.argv) == 8:
 # 6 Group that the NZB was posted in e.g. alt.binaries.x
 # 7 Status of post processing. 0 = OK, 1=failed verification, 2=failed unpack, 3=1+2
 	Logger.info("Script triggered from SABnzbd, starting autoProcessMovie...")
-	autoProcessMovie.process(sys.argv[1], sys.argv[2], sys.argv[7])
+	result = autoProcessMovie.process(sys.argv[1], sys.argv[2], sys.argv[7])
 
 # NZBGet
 elif len(sys.argv) == 4:
@@ -33,10 +33,9 @@ elif len(sys.argv) == 4:
 # 2  The original name of the NZB file
 # 3  The status of the download: 0 == successful
 	Logger.info("Script triggered from NZBGet, starting autoProcessMovie...")
-
-	autoProcessMovie.process(sys.argv[1], sys.argv[2], sys.argv[3])
+	result = autoProcessMovie.process(sys.argv[1], sys.argv[2], sys.argv[3])
 
 else:
 	Logger.warn("Invalid number of arguments received from client.")
 	Logger.info("Running autoProcessMovie as a manual run...")
-	autoProcessMovie.process('Manual Run', 'Manual Run', 0)
+	result = autoProcessMovie.process('Manual Run', 'Manual Run', 0)
