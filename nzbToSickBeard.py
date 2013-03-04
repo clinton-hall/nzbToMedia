5d4629b2531e17b755b510f12f39df007d333b78
Clinton Hall <fock_wulf@hotmail.com>
2013-03-03 17:53:08 -0800

    Get result back from autoProcessTV
    we don't do anything with it... yet.

--- nzbToSickBeard.py
+++ nzbToSickBeard.py
@@ -46,7 +46,7 @@ if len(sys.argv) == 8:
 # 6 Group that the NZB was posted in e.g. alt.binaries.x
 # 7 Status of post processing. 0 = OK, 1=failed verification, 2=failed unpack, 3=1+2
     Logger.info("Script triggered from SABnzbd, starting autoProcessTV...")
-    autoProcessTV.processEpisode(sys.argv[1], sys.argv[2], sys.argv[7])
+    result = autoProcessTV.processEpisode(sys.argv[1], sys.argv[2], sys.argv[7])
 
 # NZBGet
 elif len(sys.argv) == 4:
@@ -55,9 +55,9 @@ elif len(sys.argv) == 4:
 # 2  The original name of the NZB file
 # 3  The status of the download: 0 == successful
     Logger.info("Script triggered from NZBGet, starting autoProcessTV...")
-    autoProcessTV.processEpisode(sys.argv[1], sys.argv[2], sys.argv[3])
+    result = autoProcessTV.processEpisode(sys.argv[1], sys.argv[2], sys.argv[3])
 
 else:
     Logger.debug("Invalid number of arguments received from client.")
     Logger.info("Running autoProcessTV as a manual run...")
-    autoProcessTV.processEpisode('Manual Run', 'Manual Run', 0)
+    result = autoProcessTV.processEpisode('Manual Run', 'Manual Run', 0)
