560d31e5e12a9f9b37f153932fbb89a0814319f8
Clinton Hall <fock_wulf@hotmail.com>
2013-03-06 11:58:25 -0800

    fix indentation

--- nzbToHeadPhones.py
+++ nzbToHeadPhones.py
@@ -23,19 +23,17 @@ if len(sys.argv) == 8:
 # 5 User-defined category
 # 6 Group that the NZB was posted in e.g. alt.binaries.x
 # 7 Status of post processing. 0 = OK, 1=failed verification, 2=failed unpack, 3=1+2
-  Logger.info("Script triggered from SABnzbd, starting autoProcessMusic...")
-	result = autoProcessMusic.process(sys.argv[1], sys.argv[2], sys.argv[7])
-
+    Logger.info("Script triggered from SABnzbd, starting autoProcessMusic...")
+    result = autoProcessMusic.process(sys.argv[1], sys.argv[2], sys.argv[7])
 # NZBGet
 elif len(sys.argv) == 4:
 # NZBGet argv:
 # 1  The final directory of the job (full path)
 # 2  The original name of the NZB file
 # 3  The status of the download: 0 == successful
-	Logger.info("Script triggered from NZBGet, starting autoProcessMusic...")
-	result = autoProcessMusic.process(sys.argv[1], sys.argv[2], sys.argv[3])
-
+    Logger.info("Script triggered from NZBGet, starting autoProcessMusic...")
+    result = autoProcessMusic.process(sys.argv[1], sys.argv[2], sys.argv[3]
 else:
-	Logger.warn("Invalid number of arguments received from client.")
-	Logger.info("Running autoProcessMusic as a manual run...")
-	result = autoProcessMusic.process('Manual Run', 'Manual Run', 0)
+    Logger.warn("Invalid number of arguments received from client.")
+    Logger.info("Running autoProcessMusic as a manual run...")
+    result = autoProcessMusic.process('Manual Run', 'Manual Run', 0)
