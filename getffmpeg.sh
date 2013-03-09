73f6220674a768d320b9f80b328dff407cfc4921
Clinton Hall <fock_wulf@hotmail.com>
2013-03-08 20:33:16 -0800

    Create getffmpeg.sh
    a tool to download and install ffmpeg in linux

---
+++ getffmpeg.sh
@@ -0,0 +1,11 @@
+#!/bin/sh 
+
+git clone git://github.com/FFmpeg/FFmpeg.git
+
+cd FFmpeg
+
+./configure --enable-gpl --enable-version3 --disable-w32threads --enable-avisynth --enable-bzlib --enable-fontconfig --enable-frei0r --enable-gnutls --enable-libass --enable-libbluray --enable-libcaca --enable-libfreetype --enable-libgsm --enable-libilbc --enable-libmp3lame --enable-libopencore-amrnb --enable-libopencore-amrwb --enable-libopenjpeg --enable-libopus --enable-librtmp --enable-libschroedinger --enable-libsoxr --enable-libspeex --enable-libtheora --enable-libtwolame --enable-libvo-aacenc --enable-libvo-amrwbenc --enable-libvorbis --enable-libvpx --enable-libx264 --enable-libxavs --enable-libxvid --enable-zlib
+
+make
+
+make install
