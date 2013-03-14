ebed1104ab06338ee469697a356ecfe07a45ec70
Clinton Hall <fock_wulf@hotmail.com>
2013-03-13 21:34:23 -0700

    minor fixes

--- nzbToHeadPhones.py
+++ nzbToHeadPhones.py
@@ -3,12 +3,13 @@
 import sys
 import logging
 
+import migratecfg
 import autoProcessMusic
 from nzbToMediaEnv import *
 from nzbToMediaUtil import *
 
 #check to migrate old cfg before trying to load.
-if os.path.isfile(os.path.join(os.path.dirname(sys.argv[0]), "autoProcessMedia.cfg.sample"))
+if os.path.isfile(os.path.join(os.path.dirname(sys.argv[0]), "autoProcessMedia.cfg.sample")):
     migratecfg.migrate()
 
 nzbtomedia_configure_logging(os.path.dirname(sys.argv[0]))
