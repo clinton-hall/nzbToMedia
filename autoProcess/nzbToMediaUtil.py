95c51bba91de9419861e0f59c8d13b083d8653e0
Clinton Hall <fock_wulf@hotmail.com>
2013-03-18 23:10:41 -0700

    added rotating file handler close #93

--- autoProcess/nzbToMediaUtil.py
+++ autoProcess/nzbToMediaUtil.py
@@ -18,7 +18,7 @@ def safeName(name):
 def nzbtomedia_configure_logging(dirname):
     logFile = os.path.join(dirname, "postprocess.log")
     logging.config.fileConfig(os.path.join(dirname, "autoProcessMedia.cfg"))
-    fileHandler = logging.FileHandler(logFile, encoding='utf-8', delay=True)
+    fileHandler = logging.handlers.RotatingFileHandler(logFile, mode='a', maxBytes=20000, backupCount=1, encoding='utf-8', delay=True)
     fileHandler.formatter = logging.Formatter('%(asctime)s|%(levelname)-7.7s %(message)s', '%H:%M:%S')
     fileHandler.level = logging.DEBUG
     logging.getLogger().addHandler(fileHandler)
