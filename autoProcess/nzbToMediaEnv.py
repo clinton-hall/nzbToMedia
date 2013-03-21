dbb8a309332755e08a8d21d3fc42428dd03c85e7
Clinton Hall <fock_wulf@hotmail.com>
2013-03-21 01:24:33 -0700

    V7.0

--- autoProcess/nzbToMediaEnv.py
+++ autoProcess/nzbToMediaEnv.py
@@ -1,7 +1,7 @@
 # Make things easy and less error prone by centralising all common values
 
 # Global Constants
-VERSION = 'V6.1'
+VERSION = 'V7.0'
 
 # Constants pertinant to SabNzb
 SABNZB_NO_OF_ARGUMENTS = 8
