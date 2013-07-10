a8d65c55a2a71f3fa82b740803f13868500c0aaa
Declan McCormack <dmccormack@gmx.net>
2013-07-10 10:07:58 +0200

    Remove movie folder location from CP renamer params if CP server is running on a different server than the NZB CLient. #162

--- autoProcess/autoProcessMovie.py
+++ autoProcess/autoProcessMovie.py
@@ -222,6 +222,11 @@ def process(dirName, nzbName=None, status=0, clientAgent = "manual", download_id
     except (ConfigParser.NoOptionError, ValueError):
         transcode = 0
 
+    try:
+        remoteCPS = int(config.get("CouchPotato", "remoteCPS"))
+    except (ConfigParser.NoOptionError, ValueError):
+        remoteCPS = 0
+
     myOpener = AuthURLOpener(username, password)
 
     nzbName = str(nzbName) # make sure it is a string
@@ -257,7 +262,10 @@ def process(dirName, nzbName=None, status=0, clientAgent = "manual", download_id
         else:
             command = "renamer.scan"
             if clientAgent != "manual" and download_id != "none":
-                command = command + "/?movie_folder=" + dirName + "&downloader=" + clientAgent + "&download_id=" + download_id
+                if remoteCPS == 1:
+                    command = command + "/?downloader=" + clientAgent + "&download_id=" + download_id
+                else:
+                    command = command + "/?movie_folder=" + dirName + "&downloader=" + clientAgent + "&download_id=" + download_id
 
         url = baseURL + command
 
