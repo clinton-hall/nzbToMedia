ce0de65d6ea22630724efc17eaba906f394dedee
clinton-hall <fock_wulf@hotmail.com>
2013-09-14 21:19:11 +0930

    update to V 8.4

--- autoProcess/nzbToMediaEnv.py
+++ autoProcess/nzbToMediaEnv.py
@@ -1,7 +1,7 @@
 # Make things easy and less error prone by centralising all common values
 
 # Global Constants
-VERSION = 'V8.3'
+VERSION = 'V8.4'
 
 # Constants pertinant to SabNzb
 SABNZB_NO_OF_ARGUMENTS = 8
