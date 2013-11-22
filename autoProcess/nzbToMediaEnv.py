6473b3ac65fb64ef4174e0ca0af1dbe6262d0ef4
Clinton Hall <fock_wulf@hotmail.com>
2013-11-22 20:58:02 +1030

    Add TPB fork to failed handling. Fixes #207

--- autoProcess/nzbToMediaEnv.py
+++ autoProcess/nzbToMediaEnv.py
@@ -8,7 +8,7 @@ SABNZB_NO_OF_ARGUMENTS = 8
 
 # Constants pertaining to SickBeard Branches: 
 # extend this list to include all branches/forks that use "failed" to handle failed downloads.
-SICKBEARD_FAILED = ["failed", "TPB-failed", "Pistachitos"]
+SICKBEARD_FAILED = ["failed", "TPB-failed", "Pistachitos", "TPB"]
 # extend this list to include all branches/forks that use "dirName" not "dir"
 SICKBEARD_DIRNAME = ["failed"] 
 # extend this list to include all branches/forks that process rar and link files for torrents and therefore skip extraction and linking in TorrentToMedia.
