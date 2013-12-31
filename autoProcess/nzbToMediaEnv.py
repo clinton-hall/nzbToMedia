c13fe0c6a2c017df0d07614924c2efc7bfcbeedd
clinton-hall <fock_wulf@hotmail.com>
2013-12-31 16:02:20 +1030

    ready for merge to master. V9.0

--- autoProcess/nzbToMediaEnv.py
+++ autoProcess/nzbToMediaEnv.py
@@ -1,7 +1,7 @@
 # Make things easy and less error prone by centralising all common values
 
 # Global Constants
-VERSION = 'V8.6'
+VERSION = 'V9.0'
 
 # Constants pertinant to SabNzb
 SABNZB_NO_OF_ARGUMENTS = 8
