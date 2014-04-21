3c8b858baf6716a976f1c8c0ed400e4a5831b062
echel0n <justin.tabish@gmail.com>
2014-04-21 13:17:44 -0700

    Removed getffmpeg.sh (reverted from commit 8190a5ccae478690d95aced4baea1caef4dc3c3b)
    Added back in our improved bash script for manaully installing ffmpeg with x264 support.

---
+++ getffmpeg.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+# get ffmpeg/yasm/x264
+git clone git://source.ffmpeg.org/ffmpeg.git FFmpeg
+git clone git://github.com/yasm/yasm.git FFmpeg/yasm
+git clone git://git.videolan.org/x264.git FFmpeg/x264
+
+# compile/install yasm
+cd FFmpeg/yasm
+./autogen.sh
+./configure
+make
+make install
+cd -
+
+# compile/install x264
+cd FFmpeg/x264
+./configure --enable-static --enable-shared
+make
+make install
+ldconfig
+cd -
+
+# compile/install ffmpeg
+cd FFmpeg
+./configure --disable-asm --enable-libx264 --enable-gpl
+make install
+cd -
