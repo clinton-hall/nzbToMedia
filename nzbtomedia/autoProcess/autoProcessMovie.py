232f64c697bb4dea3393bb109dc5fbf304de0ee5
echel0n <justin.tabish@gmail.com>
2014-04-22 08:34:22 -0700

    Fixes issue #344

--- nzbtomedia/autoProcess/autoProcessMovie.py
+++ nzbtomedia/autoProcess/autoProcessMovie.py
@@ -136,7 +136,7 @@ class autoProcessMovie:
         media_id = None
         downloader = None
         release_status_old = None
-        if len(release) == 1:
+        if release:
             try:
                 release_id = release.keys()[0]
                 media_id = release[release_id]['media_id']
