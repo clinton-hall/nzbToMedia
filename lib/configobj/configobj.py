bc7847522e2df8a8bc6c3502ffb2542bb6865f28
echel0n <justin.tabish@gmail.com>
2014-04-22 07:49:35 -0700

    Fixes for lib importing

--- lib/configobj/configobj.py
+++ lib/configobj/configobj.py
@@ -19,10 +19,9 @@ import sys
 
 from codecs import BOM_UTF8, BOM_UTF16, BOM_UTF16_BE, BOM_UTF16_LE
 
-import six
-from _version import __version__
-
 # imported lazily to avoid startup performance hit if it isn't used
+from lib import six
+
 compiler = None
 
 # A dictionary mapping BOM to
