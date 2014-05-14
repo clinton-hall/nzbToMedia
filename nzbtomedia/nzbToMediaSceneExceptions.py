454c3d993e1b225d0f428765ca9864e1d9d1dc14
clinton-hall <fock_wulf@hotmail.com>
2014-05-15 06:53:57 +0930

    ome exception fixes. #385

--- nzbtomedia/nzbToMediaSceneExceptions.py
+++ nzbtomedia/nzbToMediaSceneExceptions.py
@@ -4,15 +4,15 @@ import logging
 from nzbtomedia.nzbToMediaUtil import listMediaFiles
 
 Logger = logging.getLogger()
-reverse_list = [r"\.\d{2}e\d{2}s\.", r"\.[pi]0801\.", r"\.p027\.", r"\.p084\.", r"\.p063\.", r"\b[45]62[xh]\.", r"\.yarulb\.", r"\.vtd[hp]\.", r"\.ld[.-]?bew\.", 
-                r"\.pir.?(dov|dvd|bew|db|rb)\.", r"\brdvd\.", r"\.vts\.", r"\.reneercs\.", r"\.dcv\.", r"\b(pir|mac)dh\b", r"\.reporp\.", r"\.kcaper\.", r"\.lanretni\.", 
-                r"\b3ca\b", r"\.cstn\."]
+reverse_list = [r"\.\d{2}e\d{2}s\.", r"\.[pi]0801\.", r"\.p027\.", r"\.[pi]675\.", r"\.[pi]p084\.", r"\.p063\.", r"\b[45]62[xh]\.", r"\.yarulb\.", r"\.vtd[hp]\.", 
+                r"\.ld[.-]?bew\.", r"\.pir.?(dov|dvd|bew|db|rb)\.", r"\brdvd\.", r"\.vts\.", r"\.reneercs\.", r"\.dcv\.", r"\b(pir|mac)dh\b", r"\.reporp\.", r"\.kcaper\.", 
+                r"\.lanretni\.", r"\b3ca\b", r"\.cstn\."]
 reverse_pattern = re.compile('|'.join(reverse_list), flags=re.IGNORECASE)
 season_pattern = re.compile(r"(.*\.\d{2}e\d{2}s\.)(.*)", flags=re.IGNORECASE)
 word_pattern = re.compile(r"([^A-Z0-9]*[A-Z0-9]+)")
-media_list = [r"\.s\d{2}e\d{2}\.", r"\.1080[pi]\.", r"\.720p\.", r"\.480p\.", r"\.360p\.", r"\.[xh]26[45]\b", r"\.bluray\.", r"\.[hp]dtv\.", r"\.web[.-]?dl\.",
-              r"\.(vod|dvd|web|bd|br).?rip\.", r"\.dvdr\b", r"\.stv\.", r"\.screener\.", r"\.vcd\.", r"\bhd(cam|rip)\b", r"\.proper\.", r"\.repack\.", r"\.internal\.",
-              r"\bac3\b", r"\.ntsc\.", r"\.pal\.", r"\.secam\.", r"\bdivx\b", r"\bxvid\b"]
+media_list = [r"\.s\d{2}e\d{2}\.", r"\.1080[pi]\.", r"\.720p\.", r"\.576[pi]", r"\.480[pi]\.", r"\.360p\.", r"\.[xh]26[45]\b", r"\.bluray\.", r"\.[hp]dtv\.", 
+              r"\.web[.-]?dl\.", r"\.(vod|dvd|web|bd|br).?rip\.", r"\.dvdr\b", r"\.stv\.", r"\.screener\.", r"\.vcd\.", r"\bhd(cam|rip)\b", r"\.proper\.", r"\.repack\.", 
+              r"\.internal\.", r"\bac3\b", r"\.ntsc\.", r"\.pal\.", r"\.secam\.", r"\bdivx\b", r"\bxvid\b"]
 media_pattern = re.compile('|'.join(media_list), flags=re.IGNORECASE)
 garbage_name = re.compile(r"^[a-zA-Z0-9]*$")
 char_replace = [[r"(\w)1\.(\w)",r"\1i\2"]
@@ -34,19 +34,19 @@ def replace_filename(filename, dirname, name):
     head, fileExtension = os.path.splitext(os.path.basename(filename))
     if media_pattern.search(os.path.basename(dirname)) is not None: 
         newname = os.path.basename(dirname)
-        logging.debug("Replacing file name %s with directory name %s" % (head, newname), EXCEPTION)
+        logging.debug("Replacing file name %s with directory name %s" % (head, newname))
     elif media_pattern.search(name) is not None:
         newname = name
-        logging.debug("Replacing file name %s with download name %s" % (head, newname), EXCEPTION)
+        logging.debug("Replacing file name %s with download name %s" % (head, newname))
     else:
-        logging.warning("No name replacement determined for %s" % (head), EXCEPTION)
+        logging.warning("No name replacement determined for %s" % (head))
         return 
     newfile = newname + fileExtension
     newfilePath = os.path.join(dirname, newfile)
     try:
         os.rename(filename, newfilePath)
     except Exception,e:
-        logging.error("Unable to rename file due to: %s" % (str(e)), EXCEPTION)
+        logging.error("Unable to rename file due to: %s" % (str(e)))
 
 def reverse_filename(filename, dirname, name):
     head, fileExtension = os.path.splitext(os.path.basename(filename))
@@ -66,13 +66,13 @@ def reverse_filename(filename, dirname, name):
         newname = new_words[::-1] + na_parts.group(1)[::-1]
     else:
         newname = head[::-1].title()
-    logging.debug("Reversing filename %s to %s" % (head, newname), EXCEPTION)
+    logging.debug("Reversing filename %s to %s" % (head, newname))
     newfile = newname + fileExtension
     newfilePath = os.path.join(dirname, newfile)
     try:
         os.rename(filename, newfilePath)
     except Exception,e:
-        logging.error("Unable to rename file due to: %s" % (str(e)), EXCEPTION)
+        logging.error("Unable to rename file due to: %s" % (str(e)))
 
 # dict for custom groups
 # we can add more to this list
