15632c799b98eb30c7bb19dc3bbeb49dd1186301
Clinton Hall <fock_wulf@hotmail.com>
2014-06-28 06:45:12 +0930

    Fix #447

--- nzbtomedia/autoProcess/autoProcessMusic.py
+++ nzbtomedia/autoProcess/autoProcessMusic.py
@@ -4,6 +4,7 @@ import requests
 import nzbtomedia
 
 from nzbtomedia.nzbToMediaUtil import convert_to_ascii, remoteDir
+from nzbtomedia.nzbToMediaSceneExceptions import process_all_exceptions
 from nzbtomedia import logger
 
 class autoProcessMusic:
