808c4aa3d8c2b220b600e82e677e050fc082a02c
clinton-hall <fock_wulf@hotmail.com>
2014-10-06 10:14:20 +1030

    v10.7 for merge

--- nzbtomedia/__init__.py
+++ nzbtomedia/__init__.py
@@ -275,7 +275,7 @@ def initialize(section=None):
     nzbToMediaDB.upgradeDatabase(nzbToMediaDB.DBConnection(), mainDB.InitialSchema)
 
     # Set Version and GIT variables
-    NZBTOMEDIA_VERSION = '10.6'
+    NZBTOMEDIA_VERSION = '10.7'
     VERSION_NOTIFY = int(CFG['General']['version_notify'])
     AUTO_UPDATE = int(CFG['General']['auto_update'])
     GIT_REPO = 'nzbToMedia'
