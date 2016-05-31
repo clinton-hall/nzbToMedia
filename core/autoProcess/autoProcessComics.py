c7defa37daaf0c0f79006c6eac62c324040036ec
Labrys <Labrys.git@gmail.com>
2016-05-31 11:15:14 -0400

    Fix faulty logic in conditional

--- core/autoProcess/autoProcessComics.py
+++ core/autoProcess/autoProcessComics.py
@@ -66,7 +66,7 @@ class autoProcessComics:
             return [1, "%s: Failed to post-process - Unable to connect to %s" % (section, section) ]
         for line in r.iter_lines():
             if line: logger.postprocess("%s" % (line), section)
-            if ("Post Processing SUCCESSFUL!" or "Post Processing SUCCESSFULL!")in line: success = True
+            if "Post Processing SUCCESSFUL" in line: success = True
 
         if not r.status_code in [requests.codes.ok, requests.codes.created, requests.codes.accepted]:
             logger.error("Server returned status %s" % (str(r.status_code)), section)
