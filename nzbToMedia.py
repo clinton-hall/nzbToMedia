5a7d525f6cedf1f672bfd4be2f280e2ab1e620ae
clinton-hall <fock_wulf@hotmail.com>
2016-06-18 12:38:48 +0930

    fix NZBGet script headers.

--- nzbToMedia.py
+++ nzbToMedia.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python2
 # coding=utf-8
 #
-# ##############################################################################
-# ### NZBGET POST-PROCESSING SCRIPT                                          ###
+##############################################################################
+### NZBGET POST-PROCESSING SCRIPT                                          ###
 
 # Post-Process to CouchPotato, SickBeard, NzbDrone, Mylar, Gamez, HeadPhones.
 #
@@ -10,494 +10,494 @@
 #
 # NOTE: This script requires Python to be installed on your system.
 
-# ##############################################################################
-# ### OPTIONS                                                                ###
+##############################################################################
+### OPTIONS                                                                ###
 
-# ## General
+## General
 
 # Auto Update nzbToMedia (0, 1).
 #
 # Set to 1 if you want nzbToMedia to automatically check for and update to the latest version
-# auto_update=0
+#auto_update=0
 
 # Check Media for corruption (0, 1).
 #
 # Enable/Disable media file checking using ffprobe.
-# check_media=1
+#check_media=1
 
 # Safe Mode protection of DestDir (0, 1).
 #
 # Enable/Disable a safety check to ensure we don't process all downloads in the default_downloadDirectory by mistake.
-# safe_mode=1
+#safe_mode=1
 
-# ## CouchPotato
+## CouchPotato
 
 # CouchPotato script category.
 #
 # category that gets called for post-processing with CouchPotatoServer.
-# cpsCategory=movie
+#cpsCategory=movie
 
 # CouchPotato api key.
-# cpsapikey=
+#cpsapikey=
 
 # CouchPotato host.
 #
 # The ipaddress for your CouchPotato server. e.g For the Same system use localhost or 127.0.0.1
-# cpshost=localhost
+#cpshost=localhost
 
 # CouchPotato port.
-# cpsport=5050
+#cpsport=5050
 
 # CouchPotato uses ssl (0, 1).
 #
 # Set to 1 if using ssl, else set to 0.
-# cpsssl=0
+#cpsssl=0
 
 # CouchPotato URL_Base
 #
 # set this if using a reverse proxy.
-# cpsweb_root=
+#cpsweb_root=
 
 # CouchPotato Postprocess Method (renamer, manage).
 #
 # use "renamer" for CPS renamer (default) or "manage" to call a manage update.
-# cpsmethod=renamer
+#cpsmethod=renamer
 
 # CouchPotato Delete Failed Downloads (0, 1).
 #
 # set to 1 to delete failed, or 0 to leave files in place.
-# cpsdelete_failed=0
+#cpsdelete_failed=0
 
 # CouchPotato wait_for
 #
 # Set the number of minutes to wait after calling the renamer, to check the movie has changed status.
-# cpswait_for=2
+#cpswait_for=2
 
 # Couchpotato and NZBGet are a different system (0, 1).
 #
 # Enable to replace local path with the path as per the mountPoints below.
-# cpsremote_path=0
+#cpsremote_path=0
 
-# ## SickBeard
+## SickBeard
 
 # SickBeard script category.
 #
 # category that gets called for post-processing with SickBeard.
-# sbCategory=tv
+#sbCategory=tv
 
 # SickBeard host.
 #
 # The ipaddress for your SickBeard/SickRage server. e.g For the Same system use localhost or 127.0.0.1
-# sbhost=localhost
+#sbhost=localhost
 
 # SickBeard port.
-# sbport=8081
+#sbport=8081
 
 # SickBeard username.
-# sbusername=
+#sbusername=
 
 # SickBeard password.
-# sbpassword=
+#sbpassword=
 
 # SickBeard uses ssl (0, 1).
 #
 # Set to 1 if using ssl, else set to 0.
-# sbssl=0
+#sbssl=0
 
 # SickBeard web_root
 #
 # set this if using a reverse proxy.
-# sbweb_root=
+#sbweb_root=
 
 # SickBeard watch directory.
 #
 # set this if SickBeard and nzbGet are on different systems.
-# sbwatch_dir=
+#sbwatch_dir=
 
 # SickBeard fork.
 #
 # set to default or auto to auto-detect the custom fork type.
-# sbfork=auto
+#sbfork=auto
 
 # SickBeard Delete Failed Downloads (0, 1).
 #
 # set to 1 to delete failed, or 0 to leave files in place.
-# sbdelete_failed=0
+#sbdelete_failed=0
 
 # SickBeard process method.
 #
 # set this to move, copy, hardlink, symlink as appropriate if you want to over-ride SB defaults. Leave blank to use SB default.
-# sbprocess_method=
+#sbprocess_method=
 
 # SickBeard and NZBGet are a different system (0, 1).
 #
 # Enable to replace local path with the path as per the mountPoints below.
-# sbremote_path=0
+#sbremote_path=0
 
-# ## NzbDrone
+## NzbDrone
 
 # NzbDrone script category.
 #
 # category that gets called for post-processing with NzbDrone.
-# ndCategory=tv2
+#ndCategory=tv2
 
 # NzbDrone host.
 #
 # The ipaddress for your NzbDrone/Sonarr server. e.g For the Same system use localhost or 127.0.0.1
-# ndhost=localhost
+#ndhost=localhost
 
 # NzbDrone port.
-# ndport=8989
+#ndport=8989
 
 # NzbDrone API key.
-# ndapikey=
+#ndapikey=
 
 # NzbDrone uses SSL (0, 1).
 #
 # Set to 1 if using SSL, else set to 0.
-# ndssl=0
+#ndssl=0
 
 # NzbDrone web root.
 #
 # set this if using a reverse proxy.
-# ndweb_root=
+#ndweb_root=
 
 # NzbDrone wait_for
 #
 # Set the number of minutes to wait after calling the renamer, to check the episode has changed status.
-# ndwait_for=2
+#ndwait_for=2
 
 # NzbDrone Delete Failed Downloads (0, 1).
 #
 # set to 1 to delete failed, or 0 to leave files in place.
-# nddelete_failed=0
+#nddelete_failed=0
 
 # NzbDrone and NZBGet are a different system (0, 1).
 #
 # Enable to replace local path with the path as per the mountPoints below.
-# ndremote_path=0
+#ndremote_path=0
 
-# ## HeadPhones
+## HeadPhones
 
 # HeadPhones script category.
 #
 # category that gets called for post-processing with HeadHones.
-# hpCategory=music
+#hpCategory=music
 
 # HeadPhones api key.
-# hpapikey=
+#hpapikey=
 
 # HeadPhones host.
 #
 # The ipaddress for your HeadPhones server. e.g For the Same system use localhost or 127.0.0.1
-# hphost=localhost
+#hphost=localhost
 
 # HeadPhones port.
-# hpport=8181
+#hpport=8181
 
 # HeadPhones uses ssl (0, 1).
 #
 # Set to 1 if using ssl, else set to 0.
-# hpssl=0
+#hpssl=0
 
 # HeadPhones web_root
 #
 # set this if using a reverse proxy.
-# hpweb_root=
+#hpweb_root=
 
 # HeadPhones and NZBGet are a different system (0, 1).
 #
 # Enable to replace local path with the path as per the mountPoints below.
-# hpremote_path=0
+#hpremote_path=0
 
-# ## Mylar
+## Mylar
 
 # Mylar script category.
 #
 # category that gets called for post-processing with Mylar.
-# myCategory=comics
+#myCategory=comics
 
 # Mylar host.
 #
 # The ipaddress for your Mylar server. e.g For the Same system use localhost or 127.0.0.1
-# myhost=localhost
+#myhost=localhost
 
 # Mylar port.
-# myport=8090
+#myport=8090
 
 # Mylar username.
-# myusername=
+#myusername=
 
 # Mylar password.
-# mypassword=
+#mypassword=
 
 # Mylar uses ssl (0, 1).
 #
 # Set to 1 if using ssl, else set to 0.
-# myssl=0
+#myssl=0
 
 # Mylar web_root
 #
 # set this if using a reverse proxy.
-# myweb_root=
+#myweb_root=
 
 # Mylar wait_for
 #
 # Set the number of minutes to wait after calling the force process, to check the issue has changed status.
-# myswait_for=1
+#myswait_for=1
 
 # Mylar and NZBGet are a different system (0, 1).
 #
 # Enable to replace local path with the path as per the mountPoints below.
-# myremote_path=0
+#myremote_path=0
 
-# ## Gamez
+## Gamez
 
 # Gamez script category.
 #
 # category that gets called for post-processing with Gamez.
-# gzCategory=games
+#gzCategory=games
 
 # Gamez api key.
-# gzapikey=
+#gzapikey=
 
 # Gamez host.
 #
 # The ipaddress for your Gamez server. e.g For the Same system use localhost or 127.0.0.1
-# gzhost=localhost
+#gzhost=localhost
 
 # Gamez port.
-# gzport=8085
+#gzport=8085
 
 # Gamez uses ssl (0, 1).
 #
 # Set to 1 if using ssl, else set to 0.
-# gzssl=0
+#gzssl=0
 
 # Gamez library
 #
 # move downloaded games here.
-# gzlibrary
+#gzlibrary
 
 # Gamez web_root
 #
 # set this if using a reverse proxy.
-# gzweb_root=
+#gzweb_root=
 
 # Gamez and NZBGet are a different system (0, 1).
 #
 # Enable to replace local path with the path as per the mountPoints below.
-# gzremote_path=0
+#gzremote_path=0
 
-# ## Network
+## Network
 
 # Network Mount Points (Needed for remote path above)
 #
 # Enter Mount points as LocalPath,RemotePath and separate each pair with '|'
 # e.g. mountPoints=/volume1/Public/,E:\|/volume2/share/,\\NAS\
-# mountPoints=
+#mountPoints= 
 
-# ## Extensions
+## Extensions
 
 # Media Extensions
 #
 # This is a list of media extensions that are used to verify that the download does contain valid media.
-# mediaExtensions=.mkv,.avi,.divx,.xvid,.mov,.wmv,.mp4,.mpg,.mpeg,.vob,.iso,.ts
+#mediaExtensions=.mkv,.avi,.divx,.xvid,.mov,.wmv,.mp4,.mpg,.mpeg,.vob,.iso,.ts
 
-# ## Posix
+## Posix
 
 # Niceness for external tasks Extractor and Transcoder.
 #
 # Set the Niceness value for the nice command. These range from -20 (most favorable to the process) to 19 (least favorable to the process).
-# niceness=10
+#niceness=10
 
 # ionice scheduling class (0, 1, 2, 3).
 #
 # Set the ionice scheduling class. 0 for none, 1 for real time, 2 for best-effort, 3 for idle.
-# ionice_class=2
+#ionice_class=2
 
 # ionice scheduling class data.
 #
 # Set the ionice scheduling class data. This defines the class data, if the class accepts an argument. For real time and best-effort, 0-7 is valid data.
-# ionice_classdata=4
+#ionice_classdata=4
 
-# ## Transcoder
+## Transcoder
 
 # getSubs (0, 1).
 #
 # set to 1 to download subtitles.
-# getSubs=0
+#getSubs=0
 
 # subLanguages.
 #
 # subLanguages. create a list of languages in the order you want them in your subtitles. 
-# subLanguages=eng,spa,fra
+#subLanguages=eng,spa,fra
 
 # Transcode (0, 1).
 #
 # set to 1 to transcode, otherwise set to 0.
-# transcode=0
+#transcode=0
 
 # create a duplicate, or replace the original (0, 1).
 #
 # set to 1 to cretae a new file or 0 to replace the original
-# duplicate=1
+#duplicate=1
 
 # ignore extensions.
 #
 # list of extensions that won't be transcoded.
-# ignoreExtensions=.avi,.mkv
+#ignoreExtensions=.avi,.mkv
 
 # outputFastStart (0,1).
 #
 # outputFastStart. 1 will use -movflags + faststart. 0 will disable this from being used.
-# outputFastStart=0
+#outputFastStart=0
 
 # outputVideoPath.
 #
 # outputVideoPath. Set path you want transcoded videos moved to. Leave blank to disable.
-# outputVideoPath=
+#outputVideoPath=
 
 # processOutput (0,1).
 #
 # processOutput. 1 will send the outputVideoPath to SickBeard/CouchPotato. 0 will send original files.
-# processOutput=0
+#processOutput=0
 
 # audioLanguage.
 #
 # audioLanguage. set the 3 letter language code you want as your primary audio track.
-# audioLanguage=eng
+#audioLanguage=eng
 
 # allAudioLanguages (0,1).
 #
 # allAudioLanguages. 1 will keep all audio tracks (uses AudioCodec3) where available.
-# allAudioLanguages=0
+#allAudioLanguages=0
 
 # allSubLanguages (0,1).
 #
 # allSubLanguages. 1 will keep all exisiting sub languages. 0 will discare those not in your list above.
-# allSubLanguages=0
+#allSubLanguages=0
 
 # embedSubs (0,1).
 #
 # embedSubs. 1 will embded external sub/srt subs into your video if this is supported.
-# embedSubs=1
+#embedSubs=1
 
 # burnInSubtitle (0,1).
 #
 # burnInSubtitle. burns the default sub language into your video (needed for players that don't support subs)
-# burnInSubtitle=0
+#burnInSubtitle=0
 
 # extractSubs (0,1).
 #
 # extractSubs. 1 will extract subs from the video file and save these as external srt files.
-# extractSubs=0
+#extractSubs=0
 
 # externalSubDir.
 #
 # externalSubDir. set the directory where subs should be saved (if not the same directory as the video)
-# externalSubDir=
+#externalSubDir=
 
 # outputDefault (None, iPad, iPad-1080p, iPad-720p, Apple-TV2, iPod, iPhone, PS3, xbox, Roku-1080p, Roku-720p, Roku-480p, mkv, mp4-scene-release).
 #
 # outputDefault. Loads default configs for the selected device. The remaining options below are ignored.
 # If you want to use your own profile, set None and set the remaining options below.
-# outputDefault=None
+#outputDefault=None
 
 # hwAccel (0,1).
 #
 # hwAccel. 1 will set ffmpeg to enable hardware acceleration (this requires a recent ffmpeg).
-# hwAccel=0
+#hwAccel=0
 
 # ffmpeg output settings.
-# outputVideoExtension=.mp4
-# outputVideoCodec=libx264
-# VideoCodecAllow=
-# outputVideoPreset=medium
-# outputVideoFramerate=24
-# outputVideoBitrate=800k
-# outputAudioCodec=ac3
-# AudioCodecAllow=
-# outputAudioChannels=6
-# outputAudioBitrate=640k
-# outputQualityPercent=
-# outputAudioTrack2Codec=libfaac
-# AudioCodec2Allow=
-# outputAudioTrack2Channels=2
-# outputAudioTrack2Bitrate=160k
-# outputAudioOtherCodec=libmp3lame
-# AudioOtherCodecAllow=
-# outputAudioOtherChannels=2
-# outputAudioOtherBitrate=128k
-# outputSubtitleCodec=
+#outputVideoExtension=.mp4
+#outputVideoCodec=libx264
+#VideoCodecAllow= 
+#outputVideoPreset=medium
+#outputVideoFramerate=24
+#outputVideoBitrate=800k
+#outputAudioCodec=ac3
+#AudioCodecAllow=
+#outputAudioChannels=6
+#outputAudioBitrate=640k
+#outputQualityPercent=
+#outputAudioTrack2Codec=libfaac
+#AudioCodec2Allow= 
+#outputAudioTrack2Channels=2
+#outputAudioTrack2Bitrate=160k
+#outputAudioOtherCodec=libmp3lame
+#AudioOtherCodecAllow=
+#outputAudioOtherChannels=2
+#outputAudioOtherBitrate=128k
+#outputSubtitleCodec=
 
-# ## WakeOnLan
+## WakeOnLan
 
 # use WOL (0, 1).
 #
 # set to 1 to send WOL broadcast to the mac and test the server (e.g. xbmc) on the host and port specified.
-# wolwake=0
+#wolwake=0
 
 # WOL MAC
 #
 # enter the mac address of the system to be woken.
-# wolmac=00:01:2e:2D:64:e1
+#wolmac=00:01:2e:2D:64:e1
 
 # Set the Host and Port of a server to verify system has woken.
-# wolhost=192.168.1.37
-# wolport=80
+#wolhost=192.168.1.37
+#wolport=80
 
-# ## UserScript
+## UserScript
 
 # User Script category.
 #
 # category that gets called for post-processing with user script (accepts "UNCAT", "ALL", or a defined category).
-# usCategory=mine
+#usCategory=mine
 
 # User Script Remote Path (0,1).
 #
 # Script calls commands on another system.
-# usremote_path=0
+#usremote_path=0
 
 # User Script extensions.
 #
 # What extension do you want to process? Specify all the extension, or use "ALL" to process all files.
-# user_script_mediaExtensions=.mkv,.avi,.divx,.xvid,.mov,.wmv,.mp4,.mpg,.mpeg
+#user_script_mediaExtensions=.mkv,.avi,.divx,.xvid,.mov,.wmv,.mp4,.mpg,.mpeg
 
 # User Script Path
 #
 # Specify the path to your custom script.
-# user_script_path=/nzbToMedia/userscripts/script.sh
+#user_script_path=/nzbToMedia/userscripts/script.sh
 
 # User Script arguments.
 #
 # Specify the argument(s) passed to script, comma separated in order.
 # for example FP,FN,DN, TN, TL for file path (absolute file name with path), file name, absolute directory name (with path), Torrent Name, Torrent Label/Category.
 # So the result is /media/test/script/script.sh FP FN DN TN TL. Add other arguments as needed eg -f, -r
-# user_script_param=FN
+#user_script_param=FN
 
 # User Script Run Once (0,1).
 #
 # Set user_script_runOnce = 0 to run for each file, or 1 to only run once (presumably on teh entire directory).
-# user_script_runOnce=0
+#user_script_runOnce=0
 
 # User Script Success Codes.
 #
 # Specify the successcodes returned by the user script as a comma separated list. Linux default is 0
-# user_script_successCodes=0
+#user_script_successCodes=0
 
 # User Script Clean After (0,1).
 #
 # Clean after? Note that delay function is used to prevent possible mistake :) Delay is intended as seconds
-# user_script_clean=1
+#user_script_clean=1
 
 # User Script Delay.
 #
 # Delay in seconds after processing.
-# usdelay=120
+#usdelay=120
 
-# ### NZBGET POST-PROCESSING SCRIPT                                          ###
-# ##############################################################################
+### NZBGET POST-PROCESSING SCRIPT                                          ###
+##############################################################################
 
 from __future__ import print_function
 
