713e2378935ea2c9e81869879b57e76dede5a19c
clinton-hall <fock_wulf@hotmail.com>
2018-11-04 10:12:31 +1300

    fix symlink call to SickChill. Fixes #1375

--- core/autoProcess/autoProcessTV.py
+++ core/autoProcess/autoProcessTV.py
@@ -78,7 +78,7 @@ class autoProcessTV(object):
         nzbExtractionBy = cfg.get("nzbExtractionBy", "Downloader")
         process_method = cfg.get("process_method")
         if  clientAgent == core.TORRENT_CLIENTAGENT and core.USELINK == "move-sym":
-            process_method = "Symbolic Link Reversed"
+            process_method = "symlink"
         remote_path = int(cfg.get("remote_path", 0))
         wait_for = int(cfg.get("wait_for", 2))
         force = int(cfg.get("force", 0))
