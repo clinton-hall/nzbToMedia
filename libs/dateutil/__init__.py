2eb9d9dc7ced56deea11dd2180ba23c4e74a404a
Labrys of Knossos <Labrys.git@gmail.com>
2018-12-15 00:55:30 -0500

    Update guessit to 3.0.3
    Also updates:
    - babelfish-0.5.5
    - python-dateutil-2.7.5
    - rebulk-1.0.0
    - six-1.12.0

--- libs/dateutil/__init__.py
+++ libs/dateutil/__init__.py
@@ -1,9 +1,8 @@
-"""
-Copyright (c) 2003-2010  Gustavo Niemeyer <gustavo@niemeyer.net>
+# -*- coding: utf-8 -*-
+try:
+    from ._version import version as __version__
+except ImportError:
+    __version__ = 'unknown'
 
-This module offers extensions to the standard python 2.3+
-datetime module.
-"""
-__author__ = "Gustavo Niemeyer <gustavo@niemeyer.net>"
-__license__ = "PSF License"
-__version__ = "1.5"
+__all__ = ['easter', 'parser', 'relativedelta', 'rrule', 'tz',
+           'utils', 'zoneinfo']
