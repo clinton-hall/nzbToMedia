5839257f9ba72876171e14cb9132019047bd5f95
Labrys of Knossos <Labrys.git@gmail.com>
2018-12-18 17:27:57 -0500

    Refactor autoProcessGames -> games

--- core/__init__.py
+++ core/__init__.py
@@ -44,7 +44,7 @@ from six.moves import reload_module
 
 from core import logger, nzbToMediaDB, versionCheck
 from core.auto_process.comics import Comic
-from core.auto_process.autoProcessGames import Game
+from core.auto_process.games import Game
 from core.auto_process.movies import Movie
 from core.auto_process.music import Music
 from core.auto_process.tv import TV
--- nzbToMedia.py
+++ nzbToMedia.py
@@ -630,7 +630,7 @@ import sys
 import core
 from core import logger, nzbToMediaDB
 from core.auto_process.comics import Comic
-from core.auto_process.autoProcessGames import Game
+from core.auto_process.games import Game
 from core.auto_process.movies import Movie
 from core.auto_process.music import Music
 from core.auto_process.tv import TV
