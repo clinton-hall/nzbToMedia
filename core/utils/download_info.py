7b8721b27709a86e596ba18b87e97f21df86f6a6
Labrys of Knossos <Labrys.git@gmail.com>
2019-01-05 14:53:13 -0500

    Refactor my_db -> database

--- core/utils/download_info.py
+++ core/utils/download_info.py
@@ -4,20 +4,20 @@ from six import text_type
 
 from core import logger, main_db
 
-my_db = main_db.DBConnection()
+database = main_db.DBConnection()
 
 
 def update_download_info_status(input_name, status):
     logger.db('Updating status of our download {0} in the DB to {1}'.format(input_name, status))
 
-    my_db.action('UPDATE downloads SET status=?, last_update=? WHERE input_name=?',
-                 [status, datetime.date.today().toordinal(), text_type(input_name)])
+    database.action('UPDATE downloads SET status=?, last_update=? WHERE input_name=?',
+                    [status, datetime.date.today().toordinal(), text_type(input_name)])
 
 
 def get_download_info(input_name, status):
     logger.db('Getting download info for {0} from the DB'.format(input_name))
 
-    sql_results = my_db.select('SELECT * FROM downloads WHERE input_name=? AND status=?',
-                               [text_type(input_name), status])
+    sql_results = database.select('SELECT * FROM downloads WHERE input_name=? AND status=?',
+                                  [text_type(input_name), status])
 
     return sql_results
