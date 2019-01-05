542893b30bd1a41012e4cf6396c3417f31f69615
Labrys of Knossos <Labrys.git@gmail.com>
2019-01-05 14:52:53 -0500

    Refactor download_info db connection to module variable

--- core/utils/download_info.py
+++ core/utils/download_info.py
@@ -4,11 +4,12 @@ from six import text_type
 
 from core import logger, main_db
 
+my_db = main_db.DBConnection()
+
 
 def update_download_info_status(input_name, status):
     logger.db('Updating status of our download {0} in the DB to {1}'.format(input_name, status))
 
-    my_db = main_db.DBConnection()
     my_db.action('UPDATE downloads SET status=?, last_update=? WHERE input_name=?',
                  [status, datetime.date.today().toordinal(), text_type(input_name)])
 
@@ -16,7 +17,6 @@ def update_download_info_status(input_name, status):
 def get_download_info(input_name, status):
     logger.db('Getting download info for {0} from the DB'.format(input_name))
 
-    my_db = main_db.DBConnection()
     sql_results = my_db.select('SELECT * FROM downloads WHERE input_name=? AND status=?',
                                [text_type(input_name), status])
 
