df5291fd4fddeaf2e11a975f5809f9342ed60367
Labrys of Knossos <Labrys.git@gmail.com>
2019-01-18 22:46:54 -0500

    Refactor DELUGEPORT -> DELUGE_PORT

--- core/utils/torrents.py
+++ core/utils/torrents.py
@@ -32,9 +32,9 @@ def create_torrent_class(client_agent):
 
     if client_agent == 'deluge':
         try:
-            logger.debug('Connecting to {0}: http://{1}:{2}'.format(client_agent, core.DELUGE_HOST, core.DELUGEPORT))
+            logger.debug('Connecting to {0}: http://{1}:{2}'.format(client_agent, core.DELUGE_HOST, core.DELUGE_PORT))
             tc = DelugeClient()
-            tc.connect(host=core.DELUGE_HOST, port=core.DELUGEPORT, username=core.DELUGEUSR,
+            tc.connect(host=core.DELUGE_HOST, port=core.DELUGE_PORT, username=core.DELUGEUSR,
                        password=core.DELUGEPWD)
         except Exception:
             logger.error('Failed to connect to Deluge')
