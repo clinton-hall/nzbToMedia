836a315eab6b7de57d512d77b5d29991e240b041
Clinton Hall <fock_wulf@hotmail.com>
2019-03-11 19:27:54 +1300

    Update general.py

--- tests/general.py
+++ tests/general.py
@@ -30,7 +30,7 @@ print(test)
 section = core.CFG.findsection('tv').isenabled()
 print(section)
 print(len(section))
-fork, fork_params = auto_fork('SickBeard', 'tv')
+#fork, fork_params = auto_fork('SickBeard', 'tv')
 
 if server_responding('http://127.0.0.1:5050'):
     print('CouchPotato Running')
@@ -43,30 +43,30 @@ if server_responding('http://127.0.0.1:8085'):
 if server_responding('http://127.0.0.1:8090'):
     print('Mylar Running')
 
-lan = 'pt'
-lan = Language.fromalpha2(lan)
-print(lan.alpha3)
-vidName = '/volume1/Public/Movies/A Few Good Men/A Few Good Men(1992).mkv'
-inputName = 'in.the.name.of.ben.hur.2016.bdrip.x264-rusted.nzb'
-guess = guessit.guessit(inputName)
-if guess:
+#lan = 'pt'
+#lan = Language.fromalpha2(lan)
+#print(lan.alpha3)
+#vidName = '/volume1/Public/Movies/A Few Good Men/A Few Good Men(1992).mkv'
+#inputName = 'in.the.name.of.ben.hur.2016.bdrip.x264-rusted.nzb'
+#guess = guessit.guessit(inputName)
+#if guess:
     # Movie Title
-    title = None
-    if 'title' in guess:
-        title = guess['title']
+#    title = None
+#    if 'title' in guess:
+#        title = guess['title']
     # Movie Year
-    year = None
-    if 'year' in guess:
-        year = guess['year']
-    url = 'http://www.omdbapi.com'
-    r = requests.get(url, params={'y': year, 't': title}, verify=False, timeout=(60, 300))
-    results = r.json()
-    print(results)
+#    year = None
+#    if 'year' in guess:
+#        year = guess['year']
+#    url = 'http://www.omdbapi.com'
+#    r = requests.get(url, params={'y': year, 't': title}, verify=False, timeout=(60, 300))
+#    results = r.json()
+#    print(results)
 
-subliminal.region.configure('dogpile.cache.dbm', arguments={'filename': 'cachefile.dbm'})
-languages = set()
-languages.add(lan)
-video = subliminal.scan_video(vidName)
-subtitles = subliminal.download_best_subtitles({video}, languages)
-subliminal.save_subtitles(video, subtitles[video])
+#subliminal.region.configure('dogpile.cache.dbm', arguments={'filename': 'cachefile.dbm'})
+#languages = set()
+#languages.add(lan)
+#video = subliminal.scan_video(vidName)
+#subtitles = subliminal.download_best_subtitles({video}, languages)
+#subliminal.save_subtitles(video, subtitles[video])
 del core.MYAPP
