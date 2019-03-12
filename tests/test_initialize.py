410aab4c588536aee8cb6ed8c369d277392cb15d
Clinton Hall <fock_wulf@hotmail.com>
2019-03-12 18:55:37 +1300

    improve tests (#1574)
    improve tests

--- tests/test_initialize.py
+++ tests/test_initialize.py
@@ -1,33 +1,48 @@
 #! /usr/bin/env python
 from __future__ import print_function
-
 import datetime
 import os
 import sys
-sys.path.extend(["..","."])
 
-import eol
-eol.check()
+def test_eol():
+    import eol
+    eol.check()
 
-import cleanup
-cleanup.clean(cleanup.FOLDER_STRUCTURE)
+def test_cleanup():
+    import cleanup
+    cleanup.clean(cleanup.FOLDER_STRUCTURE)
+
+def test_import_core():
+    import core
+    from core import logger, main_db
+
+def test_import_core_auto_process():
+    from core.auto_process import comics, games, movies, music, tv
+    from core.auto_process.common import ProcessResult
+
+def test_import_core_plugins():
+    from core.plugins.downloaders.nzb.utils import get_nzoid
+    from core.plugins.plex import plex_update
+
+def test_import_core_user_scripts():
+    from core.user_scripts import external_script
+
+def test_import_six():
+    from six import text_type
+
+def test_import_core_utils():
+    from core.utils import (
+        char_replace, clean_dir, convert_to_ascii,
+        extract_files, get_dirs, get_download_info,
+        update_download_info_status, replace_links,
+    )
 
 import core
 from core import logger, main_db
-from core.auto_process import comics, games, movies, music, tv
-from core.auto_process.common import ProcessResult
-from core.plugins.downloaders.nzb.utils import get_nzoid
-from core.plugins.plex import plex_update
-from core.user_scripts import external_script
-from core.utils import (
-    char_replace, clean_dir, convert_to_ascii,
-    extract_files, get_dirs, get_download_info,
-    update_download_info_status,
-)
 
-# Initialize the config
-core.initialize()
-del core.MYAPP
+def test_initial():
+    core.initialize()
+    del core.MYAPP
 
-def test_answer():
+def test_core_parameters():
     assert core.CHECK_MEDIA == 1
