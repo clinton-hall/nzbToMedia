90602bf154aa20355466f2bc7d4bcb3d7d866a56
Labrys of Knossos <Labrys.git@gmail.com>
2019-04-07 13:17:55 -0400

    Fix flake8 W293 blank line contains whitespace

--- libs/custom/synchronousdeluge/__init__.py
+++ libs/custom/synchronousdeluge/__init__.py
@@ -1,7 +1,7 @@
 # coding=utf-8
 """A synchronous implementation of the Deluge RPC protocol
    based on gevent-deluge by Christopher Rosell.
-   
+
    https://github.com/chrippa/gevent-deluge
 
 Example usage:
