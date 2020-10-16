7d59d01f25e95243dd12d60130851a384f0716a9
Clinton Hall <fock_wulf@hotmail.com>
2020-10-16 22:30:42 +1300

    Update configuration.py

--- core/plugins/downloaders/nzb/configuration.py
+++ core/plugins/downloaders/nzb/configuration.py
@@ -12,6 +12,7 @@ def configure_nzbs(config):
     nzb_config = config['Nzb']
     core.NZB_CLIENT_AGENT = nzb_config['clientAgent']  # sabnzbd
     core.NZB_DEFAULT_DIRECTORY = nzb_config['default_downloadDirectory']
+    core.NZB_NO_MANUAL = int(nzb_config['no_manuall'])
 
     configure_sabnzbd(nzb_config)
 
