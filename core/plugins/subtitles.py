288e4f07ee3035f57bf7e0e2fa9a7617e470e0ca
Henry <hp197@users.noreply.github.com>
2021-02-20 01:29:42 +0100

    Added chmod to 644 for subtitles
    I ran into problems with permissions.
    Default sublimal write files with 0600 permission.

--- core/plugins/subtitles.py
+++ core/plugins/subtitles.py
@@ -11,6 +11,8 @@ import subliminal
 import core
 from core import logger
 
+import os
+
 for provider in subliminal.provider_manager.internal_extensions:
     if provider not in [str(x) for x in subliminal.provider_manager.list_entry_points()]:
         subliminal.provider_manager.register(str(provider))
@@ -38,5 +40,9 @@ def import_subs(filename):
         video = subliminal.scan_video(filename)
         subtitles = subliminal.download_best_subtitles({video}, languages)
         subliminal.save_subtitles(video, subtitles[video])
+        
+        for subtitle in subtitles[video]:
+            subtitle_path = subliminal.subtitle.get_subtitle_path(video.name, subtitle.language)
+            os.chmod(subtitle_path, 0o644)
     except Exception as e:
         logger.error('Failed to download subtitles for {0} due to: {1}'.format(filename, e), 'SUBTITLES')
