f9dde62762f7ad4fa6a5bfca892af7263a28a3e3
clinton-hall <fock_wulf@hotmail.com>
2021-07-17 21:44:16 +1200

    update to v12.1.09 for merge

--- core/__init__.py
+++ core/__init__.py
@@ -83,7 +83,7 @@ from core.utils import (
     wake_up,
 )
 
-__version__ = '12.1.08'
+__version__ = '12.1.09'
 
 # Client Agents
 NZB_CLIENTS = ['sabnzbd', 'nzbget', 'manual']
--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ def read(*names, **kwargs):
 
 setup(
     name='nzbToMedia',
-    version='12.1.08',
+    version='12.1.09',
     license='GPLv3',
     description='Efficient on demand post processing',
     long_description="""
