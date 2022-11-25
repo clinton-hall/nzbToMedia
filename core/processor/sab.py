f888cc98b28376c9e4c3bb5b8f35d0728759faf8
Brian Sheldon <brianmsheldon@gmail.com>
2022-11-25 00:12:03 -0500

    Standardize processing

--- core/processor/sab.py
+++ core/processor/sab.py
@@ -8,14 +8,13 @@ MINIMUM_ARGUMENTS = 8
 
 
 def process_script():
-    client_agent = 'sabnzbd'
-    logger.info('Script triggered from SABnzbd Version {0}.'.format(
-        os.environ['SAB_VERSION']))
+    version = os.environ['SAB_VERSION']
+    logger.info('Script triggered from SABnzbd {0}.'.format(version))
     return nzb.process(
-        os.environ['SAB_COMPLETE_DIR'],
+        input_directory=os.environ['SAB_COMPLETE_DIR'],
         input_name=os.environ['SAB_FINAL_NAME'],
         status=int(os.environ['SAB_PP_STATUS']),
-        client_agent=client_agent,
+        client_agent='sabnzbd',
         download_id=os.environ['SAB_NZO_ID'],
         input_category=os.environ['SAB_CAT'],
         failure_link=os.environ['SAB_FAILURE_URL'],
