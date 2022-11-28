5a2bd174f4064f8baeccfaf7f09a91aa32a88b49
Labrys of Knossos <labrys@users.noreply.github.com>
2022-11-27 19:10:07 -0500

    Python 2 support dropped - removed __future__ imports are default in Python 3

--- core/plugins/downloaders/torrent/qbittorrent.py
+++ core/plugins/downloaders/torrent/qbittorrent.py
@@ -1,11 +1,3 @@
-from __future__ import (
-    absolute_import,
-    division,
-    print_function,
-    unicode_literals,
-)
-
-
 from qbittorrent import Client as qBittorrentClient
 
 import core
--- eol.py
+++ eol.py
@@ -1,12 +1,5 @@
 #!/usr/bin/env python
 
-from __future__ import (
-    absolute_import,
-    division,
-    print_function,
-    unicode_literals,
-)
-
 import datetime
 import sys
 import warnings
