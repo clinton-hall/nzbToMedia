111952a679e828abae1b2915ee21de3a8fbd7873
Clinton Hall <fock_wulf@hotmail.com>
2024-02-27 20:16:54 +1300

    Update eol.py to include 3.12

--- eol.py
+++ eol.py
@@ -28,6 +28,7 @@ def date(string, fmt='%Y-%m-%d'):
 # https://devguide.python.org/
 # https://devguide.python.org/devcycle/#devcycle
 PYTHON_EOL = {
+    (3, 12): date('2028-10-1'),
     (3, 11): date('2027-10-1'),
     (3, 10): date('2026-10-01'),
     (3, 9): date('2025-10-05'),
